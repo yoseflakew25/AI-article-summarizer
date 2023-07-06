4bd76b974cee060d4fa038e593f7223ff8ca1db5
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-07-06 13:11:47 +0300

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/yoseflakew25/AI-article-summarizer", "_blank")
           }
           className='black_btn'
         >
