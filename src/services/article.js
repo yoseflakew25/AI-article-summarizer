db1368cb005b671588ffeb0dd36561aeceefdbd1
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-07-06 13:01:38 +0300

    .

--- src/services/article.js
+++ src/services/article.js
@@ -3,31 +3,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
 
-
-const options = {
-  method: 'GET',
-  url: 'https://article-extractor-and-summarizer.p.rapidapi.com/extract',
-  params: {
-    url: 'https://time.com/6266679/musk-ai-open-letter/'
-  },
-  headers: {
-    'X-RapidAPI-Key': '43be71910fmsh5c4c0ce7aab8a99p11ee2cjsne4a147f158de',
-    'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
-  }
-};
-
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
-
-
-
-
-
-
 export const articleApi = createApi({
     reducerPath: 'articleApi',
     baseQuery: fetchBaseQuery({
